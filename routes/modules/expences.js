c825869d8772fcb60525d258eb8094d9b6921c17
alvin <ckuvn1854@gmail.com>
2023-04-30 21:36:22 +0800

    feat: add edit route

--- routes/modules/expences.js
+++ routes/modules/expences.js
@@ -15,13 +15,21 @@ router.post("/", (req, res) => {});
 
 router.get("/:expense_id/edit", (req, res) => {
   const _id = req.params.expense_id;
-  Expense.findOne({ _id })
-    .populate("categoryId")
+  Category.find()
     .lean()
-    .then((expense) => {
-      res.render("edit", { expense });
-    })
-    .catch((err) => console.log(err));
+    .then((categoryList) => {
+      Expense.findOne({ _id })
+        .populate("categoryId")
+        .lean()
+        .then((expense) => {
+          const categoryListAddJudge = categoryList.map((item) => {
+            const isCategory = item.id === expense.categoryId.id;
+            return Object.assign(item, { isCategory });
+          });
+          res.render("edit", { expense, categoryListAddJudge });
+        })
+        .catch((err) => console.log(err));
+    });
 });
 
 // 輸出
