e4cad4c3da41e1bdca0609d18be82d2947b9c577
alvin <ckuvn1854@gmail.com>
2023-05-01 15:43:51 +0800

    feat: add auth

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,13 @@ const home = require("./modules/home");
 const expenses = require("./modules/expenses");
 const users = require("./modules/users");
 
+// 驗證登入狀態
+const { authenticator } = require("../middleware/auth");
+
 // router.use
-router.use("/expenses", expenses);
+router.use("/expenses", authenticator, expenses);
 router.use("/users", users);
-router.use("/", home);
+router.use("/", authenticator, home);
 
 // 輸出
 module.exports = router;
